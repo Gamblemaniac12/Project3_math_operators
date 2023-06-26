d5446224d1ad6dd54606e6d8dc1d5b36ef98f5a4
Gamblemaniac12 <calebgambleman@gmail.com>
2023-06-25 23:41:55 -0700

    fixed some elements

--- JS/math.js
+++ JS/math.js
@@ -1,26 +1,26 @@
 function addition_Function() {
     var addition = 2 + 2;
-    document.getElementById(Math).innerHTML ="2 + 2 = " + addition;
+    document.getElementById("addition").innerHTML ="2 + 2 = " + addition;
 }
 
 function subtraction_Function() {
     var Subtraction = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+    document.getElementById("subtraction").innerHTML = "5 - 2 = " + Subtraction;
 }
 
 function multiplication() {
     var simple_Math = 6 * 8;
-    document.getElementById("Math").innerHTML = "6 * 8 = " + simple_Math
+    document.getElementById("multiplication").innerHTML = "6 * 8 = " + simple_Math
 }
 
 function division() {
     var simple_Math = 48 / 6;
-    document.getElementById("Math").innerHTML = "48 / 6 =" + simple_Math
+    document.getElementById("division").innerHTML = "48 / 6 =" + simple_Math
 }
 
 function more_Math() {
     var simple_Math = (1 + 2) * 10 / 2 -5;
-    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math
+    document.getElementById("more_Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math
 }
 
 var X = 5;
